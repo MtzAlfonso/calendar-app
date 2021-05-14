309c625c328d2f3505d0e773e3588bbd13ee7824
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-05-14 03:18:46 -0500

    Eventos para los date pickers

--- src/components/calendar/CalendarModal.jsx
+++ src/components/calendar/CalendarModal.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
+import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment';
 import Modal from 'react-modal';
+
+const now = moment().minutes(0).seconds(0).add(1, 'hour');
+const end = now.clone().add(1, 'hour');
 
 const customStyles = {
   content: {
@@ -21,6 +26,17 @@ const CalendarModal = () => {
     setIsOpen(false);
   };
 
+  const [dateStart, setDateStart] = useState(now.toDate());
+  const [dateEnd, setDateEnd] = useState(end.toDate());
+
+  const handleStartDateChange = (e) => {
+    setDateStart(e);
+  };
+
+  const handleEndDateChange = (e) => {
+    setDateEnd(e);
+  };
+
   return (
     <Modal
       className="modal pb-3"
@@ -30,21 +46,36 @@ const CalendarModal = () => {
       overlayClassName="modal-fondo"
       style={customStyles}
     >
-      <i className="ms-auto pe-3 pt-3 fas fa-times text-success close" onClick={closeModal}></i>
+      <i
+        className="ms-auto pe-3 pt-3 fas fa-times text-success close"
+        onClick={closeModal}
+      ></i>
       <h3 className="me-auto ps-3 my-3 text-uppercase fw-bold">Nuevo evento</h3>
       <form className="container">
         <div className="input-group my-4">
           <div className="input-group-text bg-primary">
             <i className="fas fa-calendar-alt text-white"></i>
           </div>
-          <input className="form-control" placeholder="Fecha inicio" />
+          <DateTimePicker
+            onChange={handleStartDateChange}
+            value={dateStart}
+            minDate={now.toDate()}
+            className="form-control"
+            format="dd / MMM / yyyy  HH:mm"
+          />
         </div>
 
         <div className="input-group my-4">
           <div className="input-group-text bg-primary">
             <i className="fas fa-calendar-alt text-white"></i>
           </div>
-          <input className="form-control" placeholder="Fecha fin" />
+          <DateTimePicker
+            onChange={handleEndDateChange}
+            value={dateEnd}
+            minDate={dateStart}
+            className="form-control"
+            format="dd / MMM / yyyy  HH:mm"
+          />
         </div>
 
         <div className="input-group my-4">
