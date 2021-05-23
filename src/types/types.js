e268fb7ca061a1c10521accd3cbb1c6267a9fbb2
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-05-23 03:25:21 -0500

    Definiendo authReducer

--- src/types/types.js
+++ src/types/types.js
@@ -1,11 +1,19 @@
 const types = {
   uiOpenModal: '[ui] Open Modal',
   uiCloseModal: '[ui] Close Modal',
+
   eventSetActive: '[event] Set active',
   eventAddNew: '[event] Add new',
   eventCleanActive: '[event] Clean active',
   eventUpdated: '[event] Event updated',
   eventDeleted: '[event] Event deleted',
+
+  authChecking: '[auth] Cheking login state',
+  authCheckingFinish: '[auth] Finish checking login state',
+  authLogin: '[auth] Login',
+  authStartRegister: '[auth] Start register',
+  authStartTokenRenew: '[auth] Start token renewal',
+  authLogout: '[auth] Logout',
 };
 
 export default types;
