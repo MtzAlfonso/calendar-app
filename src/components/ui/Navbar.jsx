3a18635fd036394138341f1ae0bd2817597a4a80
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-05-24 01:47:21 -0500

    Implementando funcionalidad de logout

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { startLogout } from '../../actions/authActions';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(startLogout());
+  };
+
+  const { name } = useSelector((state) => state.auth);
+
   return (
     <nav className="navbar navbar-dark bg-dark fixed-top">
       <div className="container-fluid">
-        <span className="navbar-brand">Alfonso</span>
-        <button className="btn btn-outline-danger px-4">
+        <span className="navbar-brand">¡Bienvenido {name}!</span>
+        <button className="btn btn-outline-danger px-4" onClick={handleLogout}>
           <i className="fas fa-sign-out-alt me-1"></i>
           Salir
         </button>
